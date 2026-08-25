1cb1529d5308ce3c5fb79fece10df390bf4ff1ce
kauanneL <kauanneL>
2026-08-24 23:05:55 -0300

    atv

--- loja.ts
+++ loja.ts
@@ -9,7 +9,7 @@ const produtos = [
     "marca": "Samsung",
     "descrição": "Smartphone com tela Super AMOLED de 6,6 polegadas e 128 GB de armazenamento.",
     "preço": 1899.90,
-    "foto": "https://m.magazineluiza.com.br/a-static/420x420/usado-galaxy-a55-256gb-azul-claro-excelente-trocafy-samsung/trocafy/329154/615113ed02d180de1dd57403f7117c7e.jpeg",
+    "foto": "https://i.zst.com.br/thumbs/12/3d/33/-1239604765.jpg",
     "quantidade": 15
   },
   {
@@ -18,7 +18,7 @@ const produtos = [
     "marca": "Lenovo",
     "descrição": "Notebook com processador Intel Core i5, 8 GB de RAM e SSD de 256 GB.",
     "preço": 3299.00,
-    "foto": "https://http2.mlstatic.com/D_Q_NP_691412-MLA99839019001_112025-F.webp",
+    "foto": "https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8MjM0NDM3fGltYWdlL3BuZ3xoYzQvaDc0LzE0MDgwNDczODU4MDc4LnBuZ3w0NTU3MDkzMDU0NTFkOGE0MWJlZDMxODMzNjQxMWE4ZGVmZjEwM2ZlMmNhYjVhNTZmZWE4NTU2MmEzNTBkYTgw/lenovo-laptop-ideapad-3-gen-6-15-amd-subseries-hero.png?width=584&height=584",
     "quantidade": 8
   },
   {
@@ -27,7 +27,7 @@ const produtos = [
     "marca": "Sony",
     "descrição": "Fone de ouvido sem fio com conexão Bluetooth e bateria de longa duração.",
     "preço": 249.90,
-    "foto": "https://http2.mlstatic.com/D_Q_NP_645386-MLA95496926110_102025-F.webp",
+    "foto": "https://m.media-amazon.com/images/I/51olNZRjn+L._AC_SX425_.jpgs",
     "quantidade": 25
   },
   {
@@ -36,7 +36,7 @@ const produtos = [
     "marca": "LG",
     "descrição": "Smart TV 4K UHD de 50 polegadas com acesso a aplicativos de streaming.",
     "preço": 2499.90,
-    "foto": "https://m.magazineluiza.com.br/a-static/420x420/smart-tv-50-samsung-uhd-4k-crystal-uhd-u8600f-un50u8600fgxzd-tizen-crystal-4k-bixby-e-alexa-3-hdmi/magazineluiza/240147400/75e1f7e592b5e38e6110ce3e702a3233.jpg",
+    "foto": "https://m.media-amazon.com/images/I/61eRNyixEfL._AC_SX425_.jpg",
     "quantidade": 6
   },
   {
@@ -45,7 +45,7 @@ const produtos = [
     "marca": "Logitech",
     "descrição": "Mouse sem fio compacto, com conexão USB e design ergonômico.",
     "preço": 79.90,
-    "foto": "https://http2.mlstatic.com/D_Q_NP_841807-MLA99435058268_112025-F.webp",
+    "foto": "https://m.media-amazon.com/images/I/61cZLujzS3L._AC_SX569_.jpg",
     "quantidade": 40
   }
 ]
